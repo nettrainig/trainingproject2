793612bc5eb5f9fd504800428763bdb4d7e0808d
rootsky <rootsky@ukr.net>
2017-10-12 02:11:05 +0300

    regular commit

---
+++ DAL/dto/ClientOrderInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.dto
+{
+    public class ClientOrderInfo
+    {
+        public int ClientID;
+        public int OrderID;
+        public decimal TotalCost;
+    }
+}
--- DAL/logic/AggregatedCalculator.cs
+++ DAL/logic/AggregatedCalculator.cs
@@ -19,9 +19,38 @@ namespace DAL.logic
             _context = context;
         }
 
-        public OrderCostInfo GetOrderCostInfo(int OrderID)
+        public OrderCostInfo GetOrderCostInfo(int orderID)
         {
-            return null;
+            return new OrderCostInfo
+            {
+                OrderID = orderID,
+                TotalCost =
+                    (from od in _context.OrderDetails
+                     join p in _context.Products on od.ProductID equals p.ID
+                     where od.OrderID == orderID
+                     select new { OrderDetailPrice = od.ProductQuantity * p.Price }).Sum(o => o.OrderDetailPrice)
+            };
+        }
+
+        public IEnumerable<ClientOrderInfo> GetClientOrdersInfo(int mostRecentCount, int clientID)
+        {
+            return
+                (from od in _context.OrderDetails
+                 join p in _context.Products on od.ProductID equals p.ID
+                 join o in _context.Orders on od.OrderID equals o.ID
+                 join c in _context.Clients on o.ClientID equals c.ID
+                 where c.ID == clientID
+                 orderby o.DateCreated
+                 select new ClientOrderInfo { ClientID = clientID, OrderID = od.OrderID, TotalCost = od.ProductQuantity * p.Price }
+                ).Take<ClientOrderInfo>(mostRecentCount);
+        }
+
+        public ClientTotalOrderInfo GetClientTotalOrdersInfo(int ClientID)
+        {
+            SqlParameter clientid = new SqlParameter("@ClientID", ClientID);
+            //var a =_context.Database.SqlQuery<ClientTotalOrderInfo>("GetClientOrders @ClientID", clientid).Single();
+            var b = _context.Database.SqlQuery<ClientTotalOrderInfo>("GetClientOrders 2").Single();
+            return b;
         }
     }
 }
